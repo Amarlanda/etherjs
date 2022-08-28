0528b35d7e627e898c53f11559cead54d3b315f3
amarlanda <al@amarlanda.com>
2022-08-28 22:27:45 +0100

    added install stuff for js_ether

---
+++ shell.sh
@@ -0,0 +1,24 @@
+npm i -g lerna
+
+# npx lerna init
+
+yarn config set workspaces-experimental true
+
+mkdir repo1 
+# mkdir repo2 
+
+# cd repo1
+
+# yarn init
+# yarn add lerna --dev
+# lerna init
+
+# cd ../repo2
+
+# yarn init
+# yarn add lerna --dev
+# lerna init
+
+
+# cd ../packages
+# yarn init
